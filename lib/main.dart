ded107a08a2452d7770d99d924aa1be9b5c839fa
unknown <abdelhadynabil142000@gmail.com>
2023-05-10 03:48:38 +0300

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:orange_project/view/auth/login_screen.dart';
+import 'package:orange_project/view/auth/otp_screen.dart';
+import 'package:orange_project/view/profile_view.dart';
+import 'package:orange_project/view/auth/register_screen.dart';
+import 'package:orange_project/view/histroy_view.dart';
+import 'package:orange_project/view/transfar_view.dart';
 import 'package:orange_project/view/bill_view.dart';
 import 'package:orange_project/view/home_view.dart';
 import 'package:orange_project/view/layout_view.dart';
 import 'package:orange_project/view/splash_view.dart';
+
+import 'constant/constant.dart';
 
 
 void main() {
@@ -23,6 +30,10 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch:Colors.deepOrange,
+        backgroundColor: Colors.black
+      ),
       home: SplashScreen(),
     );
   }
---
+++ lib/resources/app_urls.dart
@@ -0,0 +1,7 @@
+class AppUrl{
+  static var baseUrl = 'http://10.0.2.2:8080';
+  static var signInUrl = baseUrl + "/auth/signin";
+  static var signupUrl = baseUrl + "/auth/signup";
+  static var signupVerify = baseUrl + "/auth/signupVerify";
+
+}
